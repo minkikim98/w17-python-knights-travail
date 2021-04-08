765688543736a8f802dc89363f334e5db0bcdd7e
adambailey <arb5433@gmail.com>
2021-04-08 16:21:15 -0400

    “errors”

--- tree.py
+++ tree.py
@@ -18,17 +18,34 @@ class Node:
 
     @parent.setter
     def parent(self, node):
-        self._parent = node
-        node.add_child(self)
+        if (node):
+            grandparent = node.parent
+            if (grandparent):
+                grandparent.remove_child(node)
+            self._parent = node
+            node.add_child(self)
 
     def add_child(self, node):
-        if (not node in self._children):
-            node.parent = self
-        self._children.append(node)
+        if (node):
+            if (not node in self._children):
+                node.parent = self
+            self._children.append(node)
 
     def remove_child(self, child):
-        self._children.remove(child)
-        child.parent = None
+        if (child):
+            self._children.remove(child)
+            child.parent = None
+
+
+node1 = Node("root1")
+node2 = Node("root2")
+node3 = Node("root3")
+
+node3.parent = node1
+node3.parent = node2
+
+print(node1.children)
+print(node2.children)
 
     # @value.setter
 
