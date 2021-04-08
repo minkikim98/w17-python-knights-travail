ef7e5284281a0442fbe3a81c9b98f75caeb662e6
minkikim98 <minkikim98@gmail.com>
2021-04-08 14:43:11 -0700

    finished part 1

--- tree.py
+++ tree.py
@@ -18,25 +18,46 @@ class Node:
 
     @parent.setter
     def parent(self, node):
-        if (self.parent == node):
+        if self.parent == node:
             return
-        if (node):
+        if node:
             prevParent = self.parent
             self._parent = node
-            if (prevParent):
+            if prevParent:
                 prevParent.remove_child(self)
-            if (not node.children.count(self)):
+            if not node.children.count(self):
                 node.add_child(self)
         else:
             self._parent = None
 
     def add_child(self, node):
-        if (node):
-            if (not self.children.count(node)):
+        if node:
+            if not self.children.count(node):
                 self._children.append(node)
                 node.parent = self
 
     def remove_child(self, child):
-        if (child):
+        if child:
             self._children.remove(child)
             child.parent = None
+
+    def depth_search(self, value):
+        if self.value == value:
+            return self
+        else:
+            for child in self.children:
+                child_answer = child.depth_search(value)
+                if (child_answer): 
+                    return child_answer
+            return None
+
+    def breadth_search(self, value):
+        Q = [self]
+        while len(Q):
+            curr_node = Q.pop(0)
+            if curr_node.value == value:
+                return curr_node
+            else:
+                if curr_node.children:
+                    Q.extend(curr_node.children)
+        return None
